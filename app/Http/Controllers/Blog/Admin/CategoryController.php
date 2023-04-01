1e5b15ceef0a4b5e9c21770a13655d8878c07499
KakTakNeRabotaet <iliast2310@gmail.com>
2023-04-01 15:27:38 +0700

    create and edit for Categories

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\BlogCategoryCreateRequest;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoryController extends BaseController
 {
@@ -26,18 +29,35 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  BlogCategoryCreateRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        dd(__METHOD__);
+        $data = $request->input();
+        if (empty($data['slug'])){
+            $data['slug'] = Str::slug($data['title']);
+        }
+        // Создаст объект и добавит БД
+        $item = (new BlogCategory($data))->create($data);
+
+        if($item){
+            return redirect()->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успешно сохранино']);
+        } else {
+            return back()
+                ->withErrors(['msg' => "Ошибка сохраниния"])
+                ->withInput();
+        }
     }
 
     /**
@@ -68,11 +88,11 @@ class CategoryController extends BaseController
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  BlogCategoryUpdateRequest  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
         $item = BlogCategory::find($id);
         if (empty($item)){
--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -2,36 +2,44 @@
 
 @section('content')
     @php /** @var \App\Models\BlogCategory $item */ @endphp
-    <form action="{{route('blog.admin.categories.update', $item->id)}}" method="post">
+
+    @if($item->exists)
+        <form action="{{route('blog.admin.categories.update', $item->id)}}" method="post">
         @method('PATCH')
+    @else
+        <form action="{{route('blog.admin.categories.store')}}" method="post">
+    @endif
         @csrf
+
         <div class="container">
+            @php
+                /** @var \Illuminate\Support\ViewErrorBag $errors */
+            @endphp
             @if($errors->any())
-                @php
-                    /** @var \Illuminate\Support\ViewErrorBag $errors */
-                @endphp
-                <div class="row justify-content-center">
-                    <div class="col-md-11">
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            {{$errors->first()}}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                            </button>
+                    <div class="row justify-content-center">
+                        <div class="col-md-11">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{$errors->first()}}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close">
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
+                @endif
 
             @if(session('success'))
-                <div class="row justify-content-center">
-                    <div class="col-md-11">
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            {{session()->get('success')}}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                            </button>
+                    <div class="row justify-content-center">
+                        <div class="col-md-11">
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{session()->get('success')}}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close">
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
+                @endif
 
             <div class="row justify-content-center">
                 <div class="col-md-8">
